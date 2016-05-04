c33889c45763eaf9ae9f4eb02af4b8f76a6d86b1
pigu4637 <pigu4637@colorado.edu>
2016-05-04 10:48:29 -0600

    Added files via upload

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,23 +1,27 @@
 #include "FinalProject.h"
-
-QAnswer::QAnswer(int l){
-    for(int i = 0; i < tableSize;i++){
+#include <iostream>
+using namespace std;
+QAnswer::QAnswer(int l)
+{
+    for(int i = 0; i < tableSize; i++)
+    {
         hTable[i] = NULL;
     }
-    missCountx = 0;
-    questionCount = 1;
     tableSize = 50;
     questionLimit = l;
-    score = 0;
-    currentAnswer = "";
 }
 
-QAnswer::~QAnswer(){
+QAnswer::~QAnswer()
+{
 
 }
 //read the questions from the array and call build table method
-void QAnswer::readFile(std::string fileName){
+bool QAnswer::readFile(std::string fileName)
+{
     std::ifstream file(fileName);
+    bool n = true;
+    if(file.is_open())
+    {
     std::string line;
     while (std::getline(file, line))
     {
@@ -28,42 +32,58 @@ void QAnswer::readFile(std::string fileName){
         std::string* mp = &m[0];
         buildTable(h[0], m[0],mp);
     }
+    }
+    else
+    {
+        n = false;
+        std::cout << "File not found. Please download 'data.txt', thank you." << std::endl;
+    }
+    return n;
 }
 
 //build the hashtable--this functions takes the question, the answer and the multiple choice
-void QAnswer::buildTable(std::string q, std::string a,std::string m[]){
-        int index = generateHash(q);
+void QAnswer::buildTable(std::string q, std::string a,std::string m[])
+{
+    int index = generateHash(q);
 
-        if(hTable[index] == NULL){
-            hTable[index] = new Question(q, a, m);
+    if(hTable[index] == NULL)
+    {
+        hTable[index] = new Question(q, a, m);
+    }
+    else
+    {
+        Question * current = hTable[index];
+        while(current->next != NULL)
+        {
+            current = current->next;
         }
-        else{
-            Question * current = hTable[index];
-            while(current->next != NULL){
-                current = current->next;
-            }
-            Question * temp = new Question(q,a,m);
-            current->next = temp;
-        }
+        Question * temp = new Question(q,a,m);
+        current->next = temp;
+    }
 }
 /*display the random question to the user until he/she misses 3 questions or answers at least 8/10
 display the score and question count along*/
-void QAnswer::loadQuestion(int index){
+void QAnswer::loadQuestion(int index)
+{
     bool loaded = false;
-    if(hTable[index] != NULL){
+    if(hTable[index] != NULL)
+    {
         Question *q = hTable[index];
 
-        while(q->next != NULL){
-            if(q->visited == false){
+        while(q->next != NULL)
+        {
+            if(q->visited == false)
+            {
                 break;
             }
-            else{
+            else
+            {
                 q  = q->next;
             }
         }
         std::srand(std::time(0));
         random_shuffle(std::begin(q->mchoice), std::end(q->mchoice));
-        std::cout << "Score: "<< getScore() << " Question: " <<questionCount <<"/" <<questionLimit<<"\n";
+        std::cout << "Score: "<< score << " Question: " <<questionCount <<"/" <<questionLimit<<"\n";
         std::cout << q->question << "?\n";
         std::cout << "A. " << trim(q->mchoice[0]) << "\n";
         std::cout << "B. " << trim(q->mchoice[1]) << "\n";
@@ -71,41 +91,45 @@ void QAnswer::loadQuestion(int index){
         std::cout << "D. " << trim(q->mchoice[3]) << "\n";
         loaded = true;
         q->visited = true;
-        for(int i = 0; i < 4;i++){
+        for(int i = 0; i < 4; i++)
+        {
 
-           if(trim(q->mchoice[i]) == trim(q->answer)){
-                if(i==0){
+            if(trim(q->mchoice[i]) == trim(q->answer))
+            {
+                if(i==0)
+                {
                     currentAnswer = "a";
                 }
-                else if(i==1){
+                else if(i==1)
+                {
                     currentAnswer = "b";
                 }
-                else if(i==2){
+                else if(i==2)
+                {
                     currentAnswer = "c";
                 }
-                else if(i==3){
+                else if(i==3)
+                {
                     currentAnswer = "d";
                 }
-           }
+            }
         }
     }
 
-    if(!loaded){
+    if(!loaded)
+    {
         loadQuestion(randomInt(0,tableSize-1));
     }
 }
 
 //check if the user has reached the last question
-bool QAnswer::lastQuestion(){
-    if(questionCount > questionLimit){
+bool QAnswer::lastQuestion()
+{
+    if(questionCount > questionLimit)
+    {
         return true;
     }
     return false;
-}
-
-//not implemented for now.
-int QAnswer::getTimeLeft(){
-
 }
 
 //trim string from left and right to get rid of whitespace for comparison
@@ -117,72 +141,105 @@ std::string QAnswer::trim(std::string& str)
 }
 
 //answer question: accepts the answer as multiple choice--update the score and question count
-void QAnswer::answerQuestion(std::string choice){
-    if(currentAnswer == ""){
+void QAnswer::answerQuestion(std::string choice)
+{
+    if(currentAnswer == "")
+    {
         std::cout << "Questions not initialized.\n";
     }
-    else{
-        if(currentAnswer == choice){
+    else
+    {
+        if(currentAnswer == choice)
+        {
             score += 100;
         }
-        else{
+        else
+        {
             missCountx += 1;
         }
         questionCount += 1;
     }
 }
 
-//return current score
-int QAnswer::getScore(){
-    return score;
-}
-
 //check if the user has answered 3 questions already
-bool QAnswer::missCount(){
-    if(missCountx >= 3){
+bool QAnswer::missCount()
+{
+    if(missCountx >= difficulty)
+    {
         return true;
     }
     return false;
 }
 
 //display menu
-void QAnswer::menu(){
-    std::cout << "=================================QAnswer========================================\n";
-    std::cout << "Welcome to QAnswer! Answer 8/10 random questions to finish the game. Press 1 to continue.\n";
-    std::cout << "================================================================================\n\n";
+void QAnswer::menu(int i)
+{
+    if(i == 1)
+    {
+        score = 0;
+        currentAnswer = "";
+        missCountx = 0;
+        questionCount = 1;
+        std::cout << "=================================QAnswer========================================\n";
+        std::cout << "Welcome to QAnswer! Answer the most out of ten random questions to finish the game." << std::endl;
+        std::cout << "Please choose a difficulty:" << std::endl;
+        std::cout << "1. Hard" << std::endl;
+        std::cout << "2. Medium" << std::endl;
+        std::cout << "3. Easy" << std::endl;
+        std::cout << "================================================================================\n\n" << flush;
+    }
+    else
+    {
+        std::cout << "=================================QAnswer========================================\n";
+        std::cout << "Would you like to play again?" << std::endl;
+        std::cout << "1. Yes" << std::endl;
+        std::cout << "2. No" << std::endl;
+        std::cout << "================================================================================\n\n" << flush;
+    }
 }
 
 //display game over message
-void QAnswer::gameOver(){
-    std::cout << "Dayuum! Did you already answer 3 questions wrong? At least, you tried. See you next time.\n";
+void QAnswer::gameOver(bool n)
+{
+    if(!n)
+        std::cout << "Dayuum! Did you already answer " << difficulty << " questions wrong? At least, you tried.\n";
+    else
+        std::cout << "Congratulations! You have finished the game. We estimate your IQ to be: " << randomInt(100,250)<<"\n";
 }
 
 //generate random int between two numbers
-int QAnswer::randomInt(int min, int max){
-     std::random_device rd;
+int QAnswer::randomInt(int min, int max)
+{
+    std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(min, max);
     return dist(gen);
 }
 
 //helper function to ease spliting the data read from the file..very helpful
-std::vector<std::string> QAnswer::split(const char *str, char c = ' '){
+std::vector<std::string> QAnswer::split(const char *str, char c = ' ')
+{
     std::vector<std::string> result;
-    do{
+    do
+    {
         const char *begin = str;
         while(*str != c && *str)
             str++;
         result.push_back(std::string(begin, str));
-    } while (0 != *str++);
+    }
+    while (0 != *str++);
 
     return result;
 }
 
 //generate a hash based on the question
-int QAnswer::generateHash(std::string question){
+int QAnswer::generateHash(std::string question)
+{
     int sum = 0;
-    for(int i = 0; i < question.size();i++){
-        if(i%2 == 0){
+    for(int i = 0; i < question.size(); i++)
+    {
+        if(i%2 == 0)
+        {
             sum += question[i];
         }
     }
